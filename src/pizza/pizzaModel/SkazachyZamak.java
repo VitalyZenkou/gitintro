1add5a787ad0f043bf9d480503ccf31500ae299e
VitalyZenkou <blink812@gmail.com>
2019-05-03 20:09:12 +0300

    morkovka live

--- src/pizza/pizzaModel/SkazachyZamak.java
+++ src/pizza/pizzaModel/SkazachyZamak.java
@@ -2,4 +2,9 @@ package pizza.pizzaModel;
 
 public class SkazachyZamak {
 
+    private static final String nameCustomer = "Zeka Gulis";
+
+    public static String getNameCustomer() {
+        return "I like morkovka shura";
+    }
 }
